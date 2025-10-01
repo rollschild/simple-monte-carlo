921d6bd5afa2af908a3d6e5c5464f88f700f2bb1
Guangchu Shi <rollschild@protonmail.com>
2025-09-30 22:12:53 -0400

    gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rollschild/projects/simple-monte-carlo
-# Build directory: /home/rollschild/projects/simple-monte-carlo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
